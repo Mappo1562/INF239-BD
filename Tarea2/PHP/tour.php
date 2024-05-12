a9180dedbad1ae2538619384b5efd9b8c9df29d1
Alejandro <alejandro.vergara.araneda@gmail.com>
2024-05-11 22:04:47 -0400

    diseño

--- Tarea2/PHP/tour.php
+++ Tarea2/PHP/tour.php
@@ -1,51 +1,10 @@
-<?php
-
-    $servidor="localhost";
-    $usuario="root";
-    $clave="";
-    $baseDeDatos="Tarea_2";
-    $enlace= mysqli_connect($servidor,$usuario,$clave,$baseDeDatos);
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menú</title>
+    <title>Tour</title>
     <link rel="stylesheet" href="../css/bar.css" type="text/css" media="all">
-    <style>
-.formulario{
-background-color: ;
-position: absolute;
-top: 50%;
-left: 50%;
-transform: translate(-50%, -50%);
-background-color: #e4b17d;
-padding: 90px 50px;
-border-radius: 5px;
-text-align: center;
-border: 1px solid rgb(49, 49, 49);
-}
-
-input {
-	width: calc(100% - 20px);
-	padding: 9px;
-	margin: auto;
-	margin-top: 12px;
-	font-size: 16px;
-    border: none;
-}
-
-input[type='submit']{
-	background-color: #e06f72;
-	color: #fff;
-	width: calc(80% - 20px);
-	margin: 0 10%;
-	margin-top: 22px;
-    border: 1px solid rgb(49, 49, 49);
-    cursor: pointer;
-}
-    </style>
 </head>
 <body>
     <ul id="barra">
@@ -71,29 +30,5 @@ input[type='submit']{
             </svg></div><span>calificaciones</span></a></li>
         </div>
     </ul>
-
-    <div class="formulario">
-        <form action="#" name="Tarea_2" method="post">
-            <input type="number" name="rut" placeholder="rut" class="dato"><br>
-            <input type="number" name="numero_habitacion" placeholder="numero habitacion" class="dato"><br>
-            <input type="date" name="chek_in" placeholder="chek in" class="dato"><br>
-            <input type="date" name="chek_out" placeholder="chek out" class="dato"><br>
-
-            <input type="submit" name="aceptar" placeholder="aceptar">
-        </form>
-    </div>
-    <?php
-        if(isset($_POST["aceptar"])){
-            $rut_huesped=$_POST["rut"];
-            $numero_habitacion=$_POST["numero_habitacion"];
-            $f1=$_POST["chek_in"];
-            $f2=$_POST["chek_out"];
-
-            $insert = "INSERT INTO reserva VALUES ('','$rut_huesped','$numero_habitacion','$f1','$f2','')";
-            #$insert = "INSERT INTO **nombre tabla** VALUES (**valores de la tabla separados por , los vacios van '')";
-
-            $ejecuteInsert = mysqli_query($enlace,$insert);
-        }
-    ?>
 </body>
 </html>
